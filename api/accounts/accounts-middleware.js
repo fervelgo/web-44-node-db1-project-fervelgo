6bd51d55270d107cc5490ab33039ea63bbf03692
Fernando Velasco <fervelgo@gmail.com>
2021-08-09 19:21:03 -0400

    added router until post funciton

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,3 +1,5 @@
+const Account = require('./accounts-model');
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
 }
@@ -6,6 +8,20 @@ exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+function checkAccountId = (req, res, next) => {
+ try {
+   const account = Account.get(req.params.id)
+   if(!account) {
+     res.status(404).json({ message: "No account with that ID Mr."})
+   } else {
+     req.account = account
+     next()
+   }
+ } catch (err) {
+   res.status(500).json({ message: "Could not find an account under that ID Mr" })
+ }
+}
+
+module.exports = {
+  checkAccountId
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,11 +1,19 @@
+const express = require('express');
+const { checkAccountId } = require('./accounts-middleware');
+const Accounts = require('./accounts-model');
+
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  Accounts.get()
+  .then(accounts => {
+    res.json(accounts)
+  })
+  .catch(next)
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', checkAccountId, (req, res, next) => {
+    res.json(req.project)
 })
 
 router.post('/', (req, res, next) => {
